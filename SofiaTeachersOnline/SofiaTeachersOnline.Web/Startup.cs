aad9230fead6a52dd38f325d7b276795ad468976
Maggie <magi.nikolova@gmail.com>
2021-08-29 23:34:56 +0300

    Create new ASP .NET Core Web App

--- SofiaTeachersOnline/SofiaTeachersOnline.Web/Startup.cs
+++ SofiaTeachersOnline/SofiaTeachersOnline.Web/Startup.cs
@@ -1,13 +1,13 @@
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Identity;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
 using SofiaTeachersOnline.Database;
 using SofiaTeachersOnline.Database.Models;
-using SofiaTeachersOnline.Services.Services;
-using SofiaTeachersOnline.Services.Services.Contracts;
+using SofiaTeachersOnline.Web.Data;
 
 namespace SofiaTeachersOnline.Web
 {
@@ -23,24 +23,34 @@ namespace SofiaTeachersOnline.Web
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
-            services.AddControllersWithViews();
+            /*            services.AddDbContext<ApplicationDbContext>(options =>
+                            options.UseSqlServer(
+                                Configuration.GetConnectionString("DefaultConnection")));*/
 
-            services.AddScoped<IWannaBeUserService, WannaBeUserService>();
+            /*            services.AddDefaultIdentity<IdentityUser>(options => options.SignIn.RequireConfirmedAccount = true)
+                            .AddEntityFrameworkStores<ApplicationDbContext>();*/
+
+
+
 
             // Set database connection from application json file
-            services.AddDbContext<SofiaTeachersOnlineDbContext>
-                (options => options.UseSqlServer(Configuration.GetConnectionString("InitialCS")));  // TODO: Fix name InitialCS
+            services.AddDbContext<SofiaTeachersOnlineDbContext>(options =>
+                options.UseSqlServer(Configuration.GetConnectionString("DefaultConnection")));  // TODO: Fix name InitialCS
 
             // Add Identity
             // If this doesn't exit then "No service for type 'Microsoft.AspNetCore.Identity.UserManager' has been registered" error will appear
             services.AddIdentity<AppUser, AppRole>(options =>
             {
+                //options.SignIn.RequireConfirmedAccount = true;    // TODO: Add later
                 options.Password.RequireDigit = false;  // TODO: Modify to identity options
                 options.Password.RequiredUniqueChars = 0;
                 options.Password.RequireUppercase = false;
                 options.Password.RequireNonAlphanumeric = false;
             })
+            .AddRoles<AppRole>()
             .AddEntityFrameworkStores<SofiaTeachersOnlineDbContext>();
+
+            services.AddRazorPages();
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
@@ -49,25 +59,26 @@ namespace SofiaTeachersOnline.Web
             if (env.IsDevelopment())
             {
                 app.UseDeveloperExceptionPage();
+                app.UseDatabaseErrorPage();
             }
             else
             {
-                app.UseExceptionHandler("/Home/Error");
+                app.UseExceptionHandler("/Error");
                 // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
                 app.UseHsts();
             }
+
             app.UseHttpsRedirection();
             app.UseStaticFiles();
 
             app.UseRouting();
 
+            //app.UseAuthentication(); // TODO: What is it?
             app.UseAuthorization();
 
             app.UseEndpoints(endpoints =>
             {
-                endpoints.MapControllerRoute(
-                    name: "default",
-                    pattern: "{controller=Home}/{action=Index}/{id?}");
+                endpoints.MapRazorPages();
             });
         }
     }
