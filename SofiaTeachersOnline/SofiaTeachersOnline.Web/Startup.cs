2612bb0e1aaea3f60e3e17af063578a194344650
Maggie <magi.nikolova@gmail.com>
2021-08-29 23:47:35 +0300

    Scaffold Identity

--- SofiaTeachersOnline/SofiaTeachersOnline.Web/Startup.cs
+++ SofiaTeachersOnline/SofiaTeachersOnline.Web/Startup.cs
@@ -1,13 +1,11 @@
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
-using Microsoft.AspNetCore.Identity;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
 using SofiaTeachersOnline.Database;
 using SofiaTeachersOnline.Database.Models;
-using SofiaTeachersOnline.Web.Data;
 
 namespace SofiaTeachersOnline.Web
 {
